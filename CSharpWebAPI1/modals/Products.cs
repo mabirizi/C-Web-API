55323a515702cdbcf84d9c52493cf1bfd36636fd
mabirizi <mabiriziad@gmail.com>
2020-06-22 04:25:20 -0700

    CSharpWebAPI created the product modal

---
+++ CSharpWebAPI1/modals/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpWebAPI1.modals
+{
+    public class Products
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+
+        public string ProductPrice { get; set; }
+
+    }
+}
